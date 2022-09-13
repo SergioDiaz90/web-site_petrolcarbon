2682a16f33af6045819d8df6fee527a4d183f885
biodiscus <biodiscus@gmail.com>
2022-09-13 12:22:03 +0200

    chore: optimize for usage with pug-plugin

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "./styles/index.scss";
-import brand from './assets/logo_petrolcarbon.png';
-
-console.log('funciono');
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,39 @@
 const path = require('path');
 const json5 = require('json5');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
 const PugPlugin = require('pug-plugin');
 const content = require('./src/content.json');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        // using pug-plugin the entry-point is Pug template
+        // all script and styes sources must be used directly in Pug
+        index: './src/views/index.pug', // => index.html
+    },
+    resolve: {
+        // use aliases in PUG, JS, SCSS instead of relative paths
+        alias: {
+            Src: path.join(__dirname, 'src/'),
+            Views: path.join(__dirname, 'src/views/'),
+            Images: path.join(__dirname, 'src/assets/images/'),
+            Fonts: path.join(__dirname, 'src/assets/fonts/'),
+            Styles: path.join(__dirname, 'src/styles/'),
+            Scripts: path.join(__dirname, 'src/scripts/'),
+        },
+    },
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        filename: 'assets/js/[name].[contenthash:8].js', // use hashed filename for JS
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/views/index.pug',
-            filename: 'index.html',
-            minify: false
+        // enable processing of Pug files defined in entry
+        new PugPlugin({
+            pretty: true, // formatting HTML, use it only for development (works same as `minify:false` by HtmlWebpackPlugin)
+            // extract CSS to separate file
+            extractCss: {
+              // output filename of styles
+              filename: 'assets/css/[name].[contenthash:8].css',
+            },
         }),
-        new HtmlWebpackPugPlugin()
     ],
     module:{
         rules: [
@@ -31,13 +47,14 @@ module.exports = {
             {
                 test:/\.pug$/,
                 loader: PugPlugin.loader,
-                options:{
+                options: {
                     data: content.menu
                 }
             },
             {
                 test:/\.(css|sass|scss)$/i,
-                use: ['style-loader','css-loader', 'postcss-loader', 'sass-loader']
+                // don't use style-loader, all CSS should be loaded as file
+                use: ['css-loader', 'postcss-loader', 'sass-loader']
             },
             {
                 test: /\.(png|jpg|jpeg|ico)$/i,
@@ -65,8 +82,9 @@ module.exports = {
     },
     devServer: {
         static: {
-            directory: path.join(__dirname, 'src/views/**/*.*'),
+            directory: path.join(__dirname, 'dist'), // must be same as output.path
         },
+        open: true, // use this option here, not in package.json by scripts
         compress: true,
         watchFiles: {
             paths: ['src/**/*.*', 'assets/scss/**/*.*'],
