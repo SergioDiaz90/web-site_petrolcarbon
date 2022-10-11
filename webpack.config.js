a749a270e1b4ac77e3d777a9998fd1da5fd09100
Sergio <se.diaz60@gmail.com>
2022-10-11 09:21:25 -0500

    [Update]: change for public

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
         assetModuleFilename: 'src/assets/images/[name].[ext]'
     },
     plugins: [
