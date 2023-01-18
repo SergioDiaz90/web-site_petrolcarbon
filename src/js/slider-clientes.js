f0782d53a323cc67e4d84f3bc298eaa810e096ca
Sergio <se.diaz60@gmail.com>
2023-01-18 15:42:45 -0500

    [Update]: slider clientes

---
+++ src/js/slider-clientes.js
@@ -0,0 +1,32 @@
+class SliderClients {
+    constructor(selected){
+        this.move = this.move.bind(this);
+        this.slider = document.querySelector(selected);
+        this.interval = null;
+        this.cnt = 0
+        this.cntPx = 0;
+        this.start();
+        console.log('SliderClients', this.slider );
+    }
+
+    start () {
+        this.interval = window.setInterval(this.move, 3000);
+    }
+
+    move () {
+        let items = this.slider.childNodes.length;
+        console.log( 'move', items );
+        this.cnt += 1;
+        if (this.cnt > items - 4) this.cnt = 0;
+        this.moveTo(this.cnt);
+    }
+
+    moveTo (idx) {
+        if ( idx === 0 ) this.cntPx = 0;
+        this.cntPx += 25.5;
+        console.log(this.slider.querySelector(".container > img"));
+        this.slider.style.left = `-${ this.cntPx }%`;
+    }
+}
+    
+export default SliderClients;
